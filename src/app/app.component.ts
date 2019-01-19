1ca72b4d64e0eedfd6fbd9c455c6458276f16734
Jennifer Tran <Jennifer.tran101@gmail.com>
2019-01-18 23:06:37 -0600

    Added JSON Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
+import data from '../assets/data.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Toronto Waste Lookup';
 
-  find(){
+  searchTerm = "";
 
+  webData = data;
+
+  find(){
+    console.log("term is " + this.searchTerm);
   }
 }
